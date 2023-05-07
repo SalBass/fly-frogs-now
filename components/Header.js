d62a80520e97da4fd303d609b8dec399836881fb
Sal Bass <mfnkl@protonmail.com>
2023-05-07 19:04:05 -0400

    use Link for next js

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,13 @@
+import Link from 'next/link';
+
 export default function Header(props) {
   return (
     <div className="p-2 md:p-4">
-      <a href="/">
+      <Link href="/">
         <h1 className="text-4xl md:text-7xl font-bold font-display">
           Fly Frogs
         </h1>
-      </a>
+      </Link>
     </div>
   );
 }
