756186c82007d21a2a4fe5aa8086c526bd92da09
Molly Finkle <mollyfinkle@mollys-air.lan>
2024-02-09 01:05:00 -0500

    fix button wrap bug on narrow screen widths

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,6 @@
 export default function Button({onClick, children, ...rest}) {
   return (
-    <a className="rounded-full bg-white border border-black border-2 px-6 py-3 font-bold tracking-wider uppercase cursor-pointer" onClick={onClick} {...rest}>
+    <a className="rounded-full bg-white border border-black border-2 px-6 py-3 font-bold tracking-wider uppercase cursor-pointer text-center inline-block" onClick={onClick} {...rest}>
       {children}
     </a>
   );
